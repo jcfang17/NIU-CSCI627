eb5eeb34c230b903c13280234d0455b74989010b
fangjichao <jjj@jjj.com>
2023-12-03 21:58:43 -0600

    add damage visualization

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const projection = d3.geoAlbersUsa().scale(1280).translate([480, 300])
 let us;
-let currentYear = 2022;
+let currentYear;
 const yearRange = [1972, 2022];
 const yearRangeSlider = document.getElementById("yearRangeSlider");
 
@@ -43,7 +43,8 @@ function changeCurrentYear(year) {
 
 
 div1 = document.getElementById("plot1")
-let baseMap;
+let baseMap
+let cachedDetailData={}
 getBaseMap().then((svg)=>{
     baseMap = svg;
     div1.appendChild(baseMap);
@@ -64,6 +65,10 @@ function getLocationPathByYear(year) {
   return `stormdata/StormEvents_locations-ftp_v1.0_d${year}.csv`
 }
 
+/**
+ * unused
+ * @param year
+ */
 function drawStormDots(year) {
   d3.csv(getLocationPathByYear(year)).then( (data) => {
       if(baseMap===undefined) {
@@ -144,37 +149,32 @@ function drawStormDots(year) {
 }
 
 
-function drawFatalityDots(year) {
-    d3.csv(getFatalityPathByYear(year)).then((data) => {
-        if(baseMap===undefined) {
-            console.log("drawFatalityDots waiting for base map")
-            let clrId = setTimeout(()=>{drawFatalityDots(year)},1000)
-            clearTimeout(clrId)
-            console.log("drawFatalityDots done waiting")
-        }
+function getData(year){
+    cachedDetailData.promise = d3.csv(getDetailPathByYear(year)).then((data)=> {
+        let tDetFiltered = data.map(
+            d => d = {
+                xy: projection(
+                    [(parseFloat(d.BEGIN_LON) + parseFloat(d.END_LON)) / 2,
+                        (parseFloat(d.BEGIN_LAT) + parseFloat(d.BEGIN_LAT)) / 2]),
+                LOCATION: d.BEGIN_LOCATION,
+                EPISODE_ID: d.EPISODE_ID,
+                EVENT_ID: d.EVENT_ID,
+                TYPE: d.EVENT_TYPE,
+                DAMAGE_PROPERTY: parseFloat(d.DAMAGE_PROPERTY),
+                DAMAGE_CROPS: parseFloat(d.DAMAGE_CROPS),
+            })
+            .filter(d => d.xy != null)
+            .sort((a, b) => parseInt(a.EVENT_ID) - parseInt(b.EVENT_ID))
 
-        const svg = d3.select(baseMap)
-
-
-        console.log(data)
-        svg.exit()
-        svg.selectAll('circle')
-            .data(data)
-            .enter()
-            .select(d=>"circle[epid='"+d.EVENT_ID+"']")
-            // .select(d=>"circle[epid='"+d.EVENT_ID+"']")
-            .attr(d=>'fill','red')
-            .attr('r',3)
-
-
-
-
-
-
+        cachedDetailData.year = year
+        return tDetFiltered;
     })
+
+    // return d
 }
 
-let curData; //for browser debugging
+
+
 const stormType = [ "Flood", "Flash Flood","Marine Thunderstorm Wind",
     "Thunderstorm Wind", "Tornado", "Hail", "Heavy Rain",
     "Waterspout", "Marine High Wind", "Marine Strong Wind", "Debris Flow",
@@ -214,10 +214,10 @@ function drawSwathes(){
     return svg.node()
 }
 
-div2 = document.getElementById("swatch")
-div2.appendChild(drawSwathes())
 function drawStormDetailDots(year) {
-    d3.csv(getDetailPathByYear(year)).then((data) => {
+    if(cachedDetailData.year !== year){getData(year)}
+    cachedDetailData.promise.then((data) => {
+        // console.log(data)
         if (baseMap === undefined) {
             console.log("drawStormDetailDots waiting for base map")
             let clrId = setTimeout(() => {
@@ -226,21 +226,6 @@ function drawStormDetailDots(year) {
             clearTimeout(clrId)
             console.log("drawStormDetailDots done waiting")
         }
-
-        let tDetFiltered = data.map(
-            d => d = {
-                xy: projection(
-                    [(parseFloat(d.BEGIN_LON) + parseFloat(d.END_LON)) / 2,
-                        (parseFloat(d.BEGIN_LAT) + parseFloat(d.BEGIN_LAT)) / 2]),
-                LOCATION: d.BEGIN_LOCATION,
-                EPISODE_ID: d.EPISODE_ID,
-                EVENT_ID: d.EVENT_ID,
-                TYPE: d.EVENT_TYPE,
-            })
-            .filter(d => d.xy != null)
-            .sort((a, b) => parseInt(a.EVENT_ID) - parseInt(b.EVENT_ID))
-
-        curData= tDetFiltered;
 
 
 
@@ -251,7 +236,7 @@ function drawStormDetailDots(year) {
 
         svg.append('g')
             .selectAll('circle')
-            .data(tDetFiltered)
+            .data(data)
             .enter()
             .append("circle")
             .attr('cx',d=>d.xy[0])
@@ -305,7 +290,11 @@ function drawStormDetailDots(year) {
 
             })
 
+
         const swatch = d3.select("#swatch")
+        swatch.selectAll("*").remove()
+        swatch.node.appendChild(drawSwathes())
+
         swatch.selectAll("rect")
             .on("mouseover", function(event,data) {
                 d3.select(this)
@@ -326,8 +315,68 @@ function drawStormDetailDots(year) {
                     .attr("r",1)
                     .attr("stroke","none")
             })
+    })
+}
+
+function plotStackedBarChart(year) {
+    if(cachedDetailData===undefined || cachedDetailData.year!==year || baseMap===undefined) {
+        let clrId = setTimeout(() => {
+            plotStackedBarChart(year)
+        }, 500)
+        clearTimeout(clrId)
+    }
+
+    
+        
 
 
+}
+
+function plotHexBinMap(year) {
+
+    if(cachedDetailData===undefined || cachedDetailData.year!==year) {getData(year)}
+    cachedDetailData.promise.then((data) => {
+
+        const svg = d3.select(baseMap)
+        svg.selectAll("circle")
+            .transition(200)
+            .remove()
+
+        const hexbin = d3.hexbin()
+            .extent([0, 0], [960, 600])
+            .radius(18)
+            .x(d => d.xy[0])
+            .y(d => d.xy[1])
+
+        const bins = hexbin(data)
+            .map(d => (d.DAMAGE_PROP = d3.sum(d,d=>d.DAMAGE_PROPERTY),d))
+            .map(d => (d.DAMAGE_CROPS = d3.sum(d,d=>d.DAMAGE_CROPS),d))
+        // console.log(bins.map(d=>d.DAMAGE_CROPS))
+
+        const colorHex = d3.scaleSequentialLog([1,500], d3.interpolateBlues);
+        const radius = d3.scaleSqrt([d3.min(bins,d=>d.DAMAGE_PROP), d3.max(bins, d => d.DAMAGE_PROP)], [0, hexbin.radius() * Math.SQRT2]);
+
+        svg.append("g")
+            .selectAll("path")
+            .data(bins)
+            .join("path")
+            .attr("transform", d => `translate(${d.x},${d.y})`)
+            .attr("d", d => hexbin.hexagon(radius(d.DAMAGE_PROP)))
+            .attr("fill", d => colorHex(d.DAMAGE_CROPS+1))
+            .attr("stroke", d => d3.lab(colorHex(d.DAMAGE_CROPS+1)).darker())
+            .append("title")
+
+        const swatch = d3.select("#swatch")
+        swatch.selectAll("*")
+            .remove()
+        swatch.append("g")
+            .attr("transform", "translate(600,20)")
+            .append(() => Legend(d3.scaleSequentialLog([1, 500], d3.interpolateBlues), {
+                title: "Crops Damage, K$",
+                width: 260,
+                tickFormat: ".0s",
+                ticks: 5
+            }));
 
 
     })
@@ -335,3 +384,159 @@ function drawStormDetailDots(year) {
 
 
 drawStormDetailDots(2022)
+plotHexBinMap(2022)
+
+
+//-----------------helper------------------///
+function legend({color, ...options}) {
+    return Legend(color, options);
+}
+// Copyright 2021, Observable Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/color-legend
+function Legend(color, {
+    title,
+    tickSize = 6,
+    width = 320,
+    height = 44 + tickSize,
+    marginTop = 18,
+    marginRight = 0,
+    marginBottom = 16 + tickSize,
+    marginLeft = 0,
+    ticks = width / 64,
+    tickFormat,
+    tickValues
+} = {}) {
+
+    function ramp(color, n = 256) {
+        const canvas = document.createElement("canvas");
+        canvas.width = n;
+        canvas.height = 1;
+        const context = canvas.getContext("2d");
+        for (let i = 0; i < n; ++i) {
+            context.fillStyle = color(i / (n - 1));
+            context.fillRect(i, 0, 1, 1);
+        }
+        return canvas;
+    }
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .style("overflow", "visible")
+        .style("display", "block");
+
+    let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+    let x;
+
+    // Continuous
+    if (color.interpolate) {
+        const n = Math.min(color.domain().length, color.range().length);
+
+        x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+        svg.append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+    }
+
+    // Sequential
+    else if (color.interpolator) {
+        x = Object.assign(color.copy()
+                .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+            {range() { return [marginLeft, width - marginRight]; }});
+
+        svg.append("image")
+            .attr("x", marginLeft)
+            .attr("y", marginTop)
+            .attr("width", width - marginLeft - marginRight)
+            .attr("height", height - marginTop - marginBottom)
+            .attr("preserveAspectRatio", "none")
+            .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+        // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+        if (!x.ticks) {
+            if (tickValues === undefined) {
+                const n = Math.round(ticks + 1);
+                tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+            }
+            if (typeof tickFormat !== "function") {
+                tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+            }
+        }
+    }
+
+    // Threshold
+    else if (color.invertExtent) {
+        const thresholds
+            = color.thresholds ? color.thresholds() // scaleQuantize
+            : color.quantiles ? color.quantiles() // scaleQuantile
+                : color.domain(); // scaleThreshold
+
+        const thresholdFormat
+            = tickFormat === undefined ? d => d
+            : typeof tickFormat === "string" ? d3.format(tickFormat)
+                : tickFormat;
+
+        x = d3.scaleLinear()
+            .domain([-1, color.range().length - 1])
+            .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+            .selectAll("rect")
+            .data(color.range())
+            .join("rect")
+            .attr("x", (d, i) => x(i - 1))
+            .attr("y", marginTop)
+            .attr("width", (d, i) => x(i) - x(i - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", d => d);
+
+        tickValues = d3.range(thresholds.length);
+        tickFormat = i => thresholdFormat(thresholds[i], i);
+    }
+
+    // Ordinal
+    else {
+        x = d3.scaleBand()
+            .domain(color.domain())
+            .rangeRound([marginLeft, width - marginRight]);
+
+        svg.append("g")
+            .selectAll("rect")
+            .data(color.domain())
+            .join("rect")
+            .attr("x", x)
+            .attr("y", marginTop)
+            .attr("width", Math.max(0, x.bandwidth() - 1))
+            .attr("height", height - marginTop - marginBottom)
+            .attr("fill", color);
+
+        tickAdjust = () => {};
+    }
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x)
+            .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+            .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+            .tickSize(tickSize)
+            .tickValues(tickValues))
+        .call(tickAdjust)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+            .attr("x", marginLeft)
+            .attr("y", marginTop + marginBottom - height - 6)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .attr("class", "title")
+            .text(title));
+
+    return svg.node();
+}
